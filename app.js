82c34ae82f1b1c765b3a52e22b1a6330014fc324
Sara Santana <saraa.beatrizz@gmail.com>
2020-02-08 16:06:30 -0300

    Adding items to local storage and giving ID to books

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
+
 class Book {
-    constructor(title, author, year){
+    constructor(title, author, year, id){
         this.title = title;
         this.author = author;
         this.year = year;
+        this.id = id;
     }
 }
 
 class UI {
     static displayBookList(){
-        const StoredBooks = [
-            {
-                title: 'Grande Sertão: Veredas',
-                author: 'João Guimarães Rosa',
-                year: '1945'
-            }
-        ];
+       
 
-        const books = StoredBooks;
+        const books = Storage.getBooks();
 
         books.forEach((book) => UI.addBookToList(book));
     }
@@ -30,7 +26,7 @@ class UI {
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.year}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
+        <td><a href="#" class="btn btn-danger btn-sm delete" id="${book.id}">Delete</a></td>
         `;
 
         list.appendChild(row);
@@ -49,7 +45,36 @@ class UI {
     }
 
 }
+class Storage {
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }
+        else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
 
+    static addBook(book){
+        const books = Storage.getBooks();
+
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+
+    }
+
+    static removeBook(id){
+        const books = Storage.getBooks();
+        books.forEach((book, index) => {
+            if(String(book.id) === String(id)){
+                books.splice(index, 1);
+            }
+        });
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
 document.addEventListener('DOMContentLoaded', UI.displayBookList);
 document.querySelector('#book-form').addEventListener('submit', (e) => {
     e.preventDefault();
@@ -59,13 +84,23 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const year = document.querySelector('#year').value;
 
-    const book = new Book(title, author, year);
-    console.log(book);
-    UI.addBookToList(book);
-    UI.clearFields();
+    if(title === '' || author === '' || year == ''){
+        alert('Empty fields not allowed.');
+    } 
+    else {
+        const book = new Book(title, author, year, new Date().getUTCMilliseconds());
+        console.log(book);
+        UI.addBookToList(book);
+        Storage.addBook(book);
+        UI.clearFields();
+
+    }
+
+    
 });
 
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target);
+    Storage.removeBook((e.target).id);
 });
 
